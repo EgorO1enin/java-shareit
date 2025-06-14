d0031f58c45f06cd43264bdc4901491e717cb4de
e.olenin <e.olenin@expert-it.su>
2025-06-14 14:38:43 +0300

    code style fix

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -10,10 +10,13 @@ import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
     List<Item> findByOwnerId(Long ownerId);
+
     @Query("SELECT i FROM Item i WHERE i.available = true AND " +
            "(LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
            "LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%')))")
     List<Item> searchItems(String text, Pageable pageable);
+
     List<Item> findByRequestId(Long requestId);
 }
